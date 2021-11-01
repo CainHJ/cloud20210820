40a25d923b6b1f721417e57d9aa630ac00ca2718
CainHJ <124530535@qq.com>
2021-11-01 10:11:14 +0800

    测试看看

--- cloud-consumer-order80/src/main/java/com/atguigu/springCloud/config/ApplicationContextConfig.java
+++ cloud-consumer-order80/src/main/java/com/atguigu/springCloud/config/ApplicationContextConfig.java
@@ -14,6 +14,8 @@ import org.springframework.web.client.RestTemplate;
 @Configuration
 public class ApplicationContextConfig {
 
+
+
     @Bean
   //  @LoadBalanced
     public RestTemplate getRestTemplate(){
